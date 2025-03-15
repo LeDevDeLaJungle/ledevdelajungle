e27e3d67ea1c9f6381f0e13e6ff0f13e5017febd
Grifyl <prnreynaud@gmail.com>
2025-03-15 08:50:20 +0100

    feat : Ajout google ads

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'le-dev-de-la-jungle';
+  isPopupOpen = false;
+  credits = 0;
+
+  openPopup() {
+    this.isPopupOpen = true;
+  }
+
+  closePopup() {
+    this.isPopupOpen = false;
+  }
+
+  watchPub() {
+    alert(`Merci pour le coup de pouce ! :)`);
+  }
 }
